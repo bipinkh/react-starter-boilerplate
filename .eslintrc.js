aceb3e30a6dc7d430cd082e72670b2629750a5a3
bipinkh <bipinkh91@gmail.com>
2018-06-14 11:34:41 +0545

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      },
+      
+    "globals": {
+        "document": true,
+        "foo": true,
+        "window": true,
+        "Component" :true
+    }
+    
+   
+
+
+};
+
--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from "../../static/logo.svg";
+import React from 'react';
+import logo from '../../static/logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      </p>
+    </div>
+  );
 }
+
 
 export default App;
